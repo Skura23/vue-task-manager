02d89968a601c371e4c46d0de51014b17f7c11c3
skura23 <776064932@qq.com>
2017-08-04 00:11:58 +0800

    init project files

---
+++ src/components/app.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script type='text/babel'>
+export default {
+
+}
+</script>
+
+<style scoped lang='stylus' rel='stylesheet/stylus'>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+// main文件是src文件夹入口 (step1)
+// 各类公共文件插件等先在main.js引入
 import Vue from 'vue'
+// 引入app组件 (step2)
 import App from './App'
+// 获取路由器
 import router from './router'
 
 Vue.config.productionTip = false
@@ -9,6 +14,7 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  // router, 为es6语法, 表示router: router
   router,
   template: '<App/>',
   components: { App }
