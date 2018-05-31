94e714fd95e721bb206c569bc6ddd9cfbf1b4fe1
skura <776064932@qq.com>
2018-05-31 17:50:18 +0800

    Adding readme file

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-// 适用于新版vue-cli(@vue/cli)的vue调试配置文件, 博客地址如下
-// https://medium.com/@marshallswain/debugging-a-new-vue-cli-app-with-vs-code-ade4945e75f0
-// module.exports = {
-//   configureWebpack: {
-//     devtool: 'source-map'
-//   }
-// }
