8a9c198e0f0802c9ab99e4290f32ff734068a0bb
skura <776064932@qq.com>
2018-05-31 17:43:16 +0800

    dist bran

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- 这里的类名用于style,外部无法获取 -->
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// 插入各组件 (step 0)
+import TaskManager from '@/components/TaskManager/TaskManager';
+// import Test from '@/components/test/test';
+import axios from 'axios';
+// import jQuery from 'jQuery';
+// window.jQuery = jQuery
+
+export default {
+  name: 'app',
+  components: {
+    // Test,
+    TaskManager
+  }
+}
+</script>
+
+<style lang='stylus' rel='stylesheet/stylus'>
+#app
+  height 100%
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import App from '../App'
+import TaskManager from '@/components/TaskManager/TaskManager'
+// import Test from '@/components/test/test.vue'
+
+// npm安装的模块, 需要vue.use引入
+Vue.use(Router)
+
+// 配置路由
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'TaskManager',
+      component: TaskManager
+    }
+    // {
+    //   path: '/test',
+    //   name: 'Test',
+    //   component: Test
+    // }
+  ]
+})
